0a1e65f98e76e946dcdccad5d74f28d7e76601b3
Ruth Fox <ruthkangfox@gmail.com>
2022-11-22 11:03:48 -0800

    codewars Calculate Average

---
+++ class12-materials/TicTacToe/js/main.js
@@ -0,0 +1,52 @@
+//Fetch API Outline
+// document.querySelector('button').addEventListener('click', getFetch)
+
+// function getFetch(){
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {    
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
+
+
+
+
+
+// event listeners for each tile
+
+// gamestate - whos turn is currently active (use Math.rondom() to see who gets higher Number. 0-1)- active user gets image on board
+// decide who goes first
+
+// gameloop
+// PlayerTurn(){
+//     show display that it is the players move - set active game piece to be goku
+// }
+
+
+// cpuTurn(){
+//     disable event listeners to prevent player from doing anything while cpu takes their turn
+//     display gameboard turn
+//     set active game piece to vegeta
+//     add delay timer to mimick someone thinking
+//     decide how computer moves
+//     if tile  =   10- 
+//                  -10 
+//                  ---
+        
+// }
+            
+// checkForWinner()
+
+// p1Victory{
+
+// }
+
+// p2Victory{
+
+// }
+
+
